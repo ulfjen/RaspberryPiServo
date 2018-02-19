43361b1e36a848a464d4570044e2b091f147cf31
Ulf Jennehag <ulf.jennehag@gmail.com>
2018-02-20 00:48:20 +0100

    Uppdaterad kod som kompilerar

--- servodriver.c
+++ servodriver.c
@@ -25,15 +25,15 @@ int main()
 		// Start timer
 		clock_gettime(CLOCK_MONOTONIC_RAW,&start_time);
 		// Sleep one ms
-		sleep(1);
+		delay(1);
 		clock_gettime(CLOCK_MONOTONIC_RAW,&end_time);
 
 		diff = (end_time.tv_sec - start_time.tv_sec)*(long)1e9 + (end_time.tv_nsec-start_time.tv_nsec);
 
-		printf("Is 1ms:%l\n",diff);
+		printf("Is 1ms:%ld \n",diff);
 
 		delay(1000);
-
+		
 	}
 
 	return 0;
