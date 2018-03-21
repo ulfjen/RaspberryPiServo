74016ce47c32e7c9ae3c411ed6321a32db2c8da1
Ulf Jennehag <ulf.jennehag@gmail.com>
2018-03-21 19:08:42 +0000

    Working!
    Driver works!

--- servodriver.c
+++ servodriver.c
@@ -7,32 +7,63 @@
 #include <stdio.h>
 #include <time.h>
 #include <wiringPi.h>
-
+#include <stdlib.h>
 
 
 int main()
 {
 	struct timespec start_time, end_time;
-	long diff;
+	//long diff;
+	long i=2000000;
+	int  d=0,diff;
 	// WiringPi
 	wiringPiSetup () ;
 	pinMode(0, OUTPUT);
+	srand(time(NULL));
 
 	// ==============
 
+	diff = 50000;
 	while (1)
 	{
+
 		// Start timer
 		clock_gettime(CLOCK_MONOTONIC_RAW,&start_time);
 		// Sleep one ms
-		sleep(1);
+		
 		clock_gettime(CLOCK_MONOTONIC_RAW,&end_time);
+		digitalWrite(0,HIGH);
 
-		diff = (end_time.tv_sec - start_time.tv_sec)*(long)1e9 + (end_time.tv_nsec-start_time.tv_nsec);
+		while (i >(end_time.tv_sec - start_time.tv_sec)*(long)1e9 + (end_time.tv_nsec-start_time.tv_nsec))
+		{
+			clock_gettime(CLOCK_MONOTONIC_RAW,&end_time);
+		}
 
-		printf("Is 1ms:%l\n",diff);
+		digitalWrite(0,LOW);
+		//diff = (end_time.tv_sec - start_time.tv_sec)*(long)1e9 + (end_time.tv_nsec-start_time.tv_nsec);
 
-		delay(1000);
+		//printf("Is 1ms:%ld\n",diff);
+
+		
+		i=i+diff;
+		
+		//d= rand()%3;
+		
+		delay(15+d);
+
+
+		if (i>2000000)
+		{
+		  //return 0;
+		  diff = -50000;
+
+		}
+		if (i<1000000)
+		{
+		  diff = 50000;
+		}
+
+		
 
 	}
 
